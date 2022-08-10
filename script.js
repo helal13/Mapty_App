e1d1c759e1788a2934af8a9d13022526752ba882
helal <mohammedhelalca@gmail.com>
2022-08-10 20:08:11 +0200

    Adding the geolocation and map

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+// Getting the user Location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const coords = [position.coords.latitude, position.coords.longitude];
+
+      // Adding the map from Leaflet
+      const map = L.map("map").setView(coords, 13);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on("click", (mapEvent) => {
+        const { lat, lng } = mapEvent.latlng;
+        const markerCoords = [lat, lng];
+        L.marker(markerCoords, { riseOnHover: true })
+          .addTo(map)
+          .bindPopup("Workout")
+          .openPopup();
+      });
+    },
+    function () {
+      alert("Pleaseallow app to use your location");
+    }
+  );
+}
